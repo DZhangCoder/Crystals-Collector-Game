580eee0aae0f0d0426ca1f9e3b4190f6a6bcfb1d
Dongdong Zhang <45675253+DZhangCoder@users.noreply.github.com>
2019-03-16 23:17:36 -0700

    updating js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,43 +4,44 @@ var wins = 0;
 var losses = 0;
 
 // define a function that generates a random number 
-function randomNumberGen(x,y) {
-    var num = (Math.floor(Math.random() * x) + y);
-    return num;
+function randomNumberGen(x, y) {
+    return (Math.floor(Math.random() * x) + y);
 }
 
-// generate a random number (19-120)
-var randomNumber = randomNumberGen(102,19);
-console.log(randomNumber);
-$("#randomNumber").text(randomNumber);
+// define a function that reset the game 
+function reset() {
+    // If user wins or loses, reset the game. 
+    // generate a random number between 19 and 120
+    randomNumber = randomNumberGen(102, 19);
+    $("#randomNumber").text(randomNumber);
 
-// get the random value of red gem (1-12)
-$("#redGem").val(randomNumberGen(12,1));
-console.log($("#redGem").val());
+    // reset the total score to 0 
+    total_score = 0;
+    $("#totalScore").text(total_score);
 
-// get the random value of blue gem (1-12)
-$("#blueGem").val(randomNumberGen(12,1));
-console.log($("#blueGem").val());
+    // reset the value of each gem 
+    $("#redGem").val(randomNumberGen(12, 1));
+    $("#blueGem").val(randomNumberGen(12, 1));
+    $("#yellowGem").val(randomNumberGen(12, 1));
+    $("#greenGem").val(randomNumberGen(12, 1));
+}
 
-// get the random value of yellow gem (1-12)
-$("#yellowGem").val(randomNumberGen(12,1));
-console.log($("#yellowGem").val());
-
-// get the random value of green gem (1-12)
-$("#greenGem").val(randomNumberGen(12,1));
-console.log($("#greenGem").val());
-
+reset();
 // when click on the image, show the value of each gem. 
 $(".gem").on("click", function () {
     total_score = total_score + parseInt($(this).val());
     $("#totalScore").text(total_score);
-    // the rules of wins and losses 
+    // the rules of wins
     if (total_score === randomNumber) {
         wins++;
         $("#wins").text("Wins: " + wins);
+        reset();
     }
+
+    // the rules of losses
     else if (total_score > randomNumber) {
         losses++;
         $("#losses").text("Losses: " + losses);
+        reset();
     }
 }) 
